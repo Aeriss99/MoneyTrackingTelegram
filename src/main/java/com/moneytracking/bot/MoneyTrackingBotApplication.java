0025ff6571b744f7bc4e7113e68efe4ad9ce9978
lingz69 <linggaardyansyh1@gmail.com>
2026-09-03 22:23:29 +0700

    fix: remove dotenv and use standard cloud environment injection

--- src/main/java/com/moneytracking/bot/MoneyTrackingBotApplication.java
+++ src/main/java/com/moneytracking/bot/MoneyTrackingBotApplication.java
@@ -1,6 +1,5 @@
 package com.moneytracking.bot;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MoneyTrackingBotApplication {
 
 	public static void main(String[] args) {
-		// Load .env file
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-		
 		SpringApplication.run(MoneyTrackingBotApplication.class, args);
 	}
 
